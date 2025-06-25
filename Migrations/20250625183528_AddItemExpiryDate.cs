1e0fd85f3a57a2b7e8d0893be98a34dba97dc000
Gunef ŞAHİN <gunefsahinn@gmail.com>
2025-06-25 21:47:47 +0300

    Add ItemExpiryDate

---
+++ Migrations/20250625183528_AddItemExpiryDate.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EnvantoryManagement.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddItemExpiryDate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "ExpiryDate",
+                table: "Items",
+                type: "datetime2",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ExpiryDate",
+                table: "Items");
+        }
+    }
+}
